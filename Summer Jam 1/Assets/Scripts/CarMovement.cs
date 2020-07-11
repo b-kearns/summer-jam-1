c874084c45a5bfdfa37a6608fe075064a1d13844
b-kearns <46977364+b-kearns@users.noreply.github.com>
2020-07-10 22:03:56 -0700

    Movement and Camera Update

---
+++ Summer Jam 1/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private Rigidbody body;
+
+    private Vector3 movementVector;
+
+    public float acceleration = 10f;
+    public float rotationSpd = 0.5f;
+
+    private float movementDirection;
+    private float rotationDirection;
+
+    private float currentSpd;
+    private int isMoving;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        movementVector = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddRelativeForce(Vector3.forward * movementDirection * acceleration);
+
+        if (body.velocity.magnitude != 0) { transform.Rotate(Vector3.up * rotationDirection * rotationSpd); }
+    }
+
+    public void SetAcceleration(float inputValue)
+    {
+        movementDirection = inputValue;
+    }
+
+    public void SetRotation(float inputValue)
+    {
+        rotationDirection = inputValue;
+    }
+}
---
+++ Summer Jam 1/Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+using static UnityEngine.InputSystem.InputAction;
+using static UnityEngine.InputSystem.InputAction.CallbackContext;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [Header("Player Prefab")]
+    [SerializeField] private GameObject playerInstance0 = null;
+    [SerializeField] private GameObject playerInstance1 = null;
+    [SerializeField] private GameObject playerInstance2 = null;
+    public int playerIndex;
+
+    private PlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        this.playerIndex = playerInput.playerIndex;
+        this.name = "Player Controller :: " + this.playerIndex;
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    public void OnAccelerateCar1(CallbackContext context)
+    {
+        playerInstance0.GetComponent<CarMovement>().SetAcceleration(context.ReadValue<float>());
+    }
+
+    public void OnRotateCar1(CallbackContext context)
+    {
+        playerInstance0.GetComponent<CarMovement>().SetRotation(context.ReadValue<float>());
+    }
+
+    public void OnAccelerateCar2(CallbackContext context)
+    {
+        playerInstance1.GetComponent<CarMovement>().SetAcceleration(context.ReadValue<float>());
+    }
+
+    public void OnRotateCar2(CallbackContext context)
+    {
+        playerInstance1.GetComponent<CarMovement>().SetRotation(context.ReadValue<float>());
+    }
+
+    public void OnAccelerateCar3(CallbackContext context)
+    {
+        playerInstance2.GetComponent<CarMovement>().SetAcceleration(context.ReadValue<float>());
+    }
+
+    public void OnRotateCar3(CallbackContext context)
+    {
+        playerInstance2.GetComponent<CarMovement>().SetRotation(context.ReadValue<float>());
+    }
+
+    
+
+}
